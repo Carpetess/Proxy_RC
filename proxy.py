f0b26b1c34643e2c503d74cb3bd5caf676f2e51c
AndreFilipeee <aas.filipe@campus.fct.unl.pt>
2024-12-12 17:51:33 +0000

    Updated and finished code

--- proxy.py
+++ proxy.py
@@ -1,11 +1,59 @@
+import threading
+import time
+from datetime import datetime
 from socket import *
 import sys
 
 import requests
-from urllib.parse import urlparse
+cache = {}
+cache_lock = threading.Lock()
+
+def get_age(headers):
+    for header in headers:
+        if header.split()[0] == "Date":
+            return header
+
+def proxyServer(tcpCliSock):
+    global cache, cache_lock
+    request = (tcpCliSock.recv(1024).decode())
+    if not request:
+        return
+    request_url = request.split("\r\n")[0].split()[1]
+    if request.split("\r\n")[0].split()[0] != "GET":
+        tcpCliSock.send("400".encode())
+        return
+
+    headers = requests.head(request_url)
+
+    with cache_lock:
+        if request_url in cache:
+            request_age = get_age(headers)
+            cache_age = cache.get(request_url)[1]
+            if request_age != cache_age:
+                request_obj = requests.get(request_url)
+                cache.update({request_url: (request_obj, request_age)})
+            else:
+                request_obj = cache.get(request_url)[0]
+        else:
+            request_obj = requests.get(request_url)
+            cache[request_url] = (request_obj, get_age(request_obj.headers))
+    if request_obj.status_code >= 400:
+        tcpCliSock.send(request_obj.status_code.to_bytes())
+        return
+    print('Connected to original destination')
+
+    print('received reply from http server')
+    response_headers = "\r\n".join(f"{key}: {value}" for key, value in request_obj.headers.items())
+    tcpCliSock.sendall(f"HTTP/1.1 {request_obj.status_code} {request_obj.reason}\r\n".encode())
+    tcpCliSock.sendall(f"{response_headers}\r\n\r\n".encode())
+    tcpCliSock.sendall(request_obj.content)
+
+    print('reply forwarded to client')
+    # Close the client socket
+    tcpCliSock.close()
 
 
-def proxyServer(portNo):
+def thread_handler(portNo):
     tcpSerSock = socket(AF_INET, SOCK_STREAM)
     tcpSerSock.bind(("0.0.0.0", portNo))
     tcpSerSock.listen(5)
@@ -13,72 +61,18 @@ def proxyServer(portNo):
         print('Ready to serve')
         try:
             tcpCliSock, addr = tcpSerSock.accept()
+            tid = threading.Thread(target=proxyServer, args=[tcpCliSock])
+            tid.start()
         except KeyboardInterrupt:
             print("proxy exiting!")
             break
-        print('Received a connection from:')#, addr)
-        #Fill in start.
-        request_rec = tcpCliSock.recv(1024).decode()
-        request = request_rec.split("\r\n")
-        method = request[0].split(" ")[0]
-        url = request[0].split(" ")[1]
-        headers, body = get_headers_body(request)
-
-        print('Connecting to original destination')
-        hostname = None
-        for header in headers:
-            if header.split(" ")[0] == "Host:":
-                hostname = header.split(" ")[1]
-        if hostname is not None:
-            serverSocket = socket(AF_INET, SOCK_STREAM)
-            serverSocket.connect((hostname, 80))
-        else:
-            reply = "4XX"
-            break
-        # Fill in start.
-        # Create a socket and connect to port 80 of the host in the URL
-        # If connect fails prepare reply with error message 4XX
-        # Fill in end.
-        print('Connected to original destination')
-        serverSocket.send(request_rec.encode())
-        response_rec = serverSocket.recv(4096)
-        response = response_rec.decode().split("\r\n")
-        print(response)
-        status_numeric = response[0].split(" ")[1]
-        status_textual = response[0].split(" ")[2]
-        header_response, body_response = get_headers_body(response)
-        # Fill in start.
-        # Receive reply from server. Separate it in status (numeric
-        # and textual, header lines and body (in the case of POST)
-        # Fill in end.
-        print('received reply from http server')
-        tcpCliSock.send(response_rec)
-        # Fill in start.
-        # forward server’s reply to client
-        # Fill in end.
-        print('reply forwarded to client')
-        # Close the client socket
-        tcpCliSock.close()
-        # Proxy server is terminated by control C.
-    tcpSerSock.close()
-
-def get_headers_body(request):
-    headers = []
-    body = []
-    for item in request:
-        if item != '' and item != request[0]:
-            if item.split(" ")[0][-1] == ":":
-                headers.append(item)
-            else:
-                body.append(item)
-
-    return headers, body
+        print('Received a connection from: ', addr)
 
 if __name__ == "__main__":
     # python proxyServer.py serverTCPPort
-    if len(sys.argv)!=2:
+    if len(sys.argv) != 2:
         print("Usage: python3 proxyServer.py proxyTCPPort")
         sys.exit(1)
     else:
-        proxyServer(int(sys.argv[1]))
+        thread_handler(int(sys.argv[1]))
         sys.exit(0)
